d9357365894744725bbc4b72ded5e9f1b2be6d42
Crogen0407 <mymcoding0407@gmail.com>
2024-01-24 00:06:56 +0900

    feat : CustomHierarchy

--- Assets/Crogen/CustomHierarchy/Editor/CustomHierarchy.cs
+++ Assets/Crogen/CustomHierarchy/Editor/CustomHierarchy.cs
@@ -9,6 +9,7 @@ using UnityEditor;
 #endif
 public class CustomHierarchy : MonoBehaviour
 {
+    private static Vector2 offset = new Vector2(16.8f, 0);
     static CustomHierarchy()
     {
         EditorApplication.hierarchyWindowItemOnGUI += HandleHierarchyOnGUI;
@@ -16,32 +17,37 @@ public class CustomHierarchy : MonoBehaviour
 
     private static void HandleHierarchyOnGUI(int instanceID, Rect selectionRect)
     {
-        FontStyle styleFont = FontStyle.Normal; //default font style
-
         var obj = EditorUtility.InstanceIDToObject(instanceID);
+
+        Color backgroundColor = Color.white;
+        Color textColor = Color.white;
+        Texture2D texture = null;
         if (obj != null)
         {
             if (obj.name.StartsWith("="))
             {
-                GUI.color = Color.green;
+                backgroundColor = new Color(0,1,1,0.5f);
+                textColor = new Color(0,1,0.5f,1);
             }
-            else
+
+            if (backgroundColor != Color.white)
             {
-                GUI.color = Color.white;
-            }    
+                Rect offsetRect = new Rect(selectionRect.position + offset, selectionRect.size);
+                Rect bgRect = new Rect(selectionRect.x, selectionRect.y, selectionRect.width + 50,
+                    selectionRect.height);
+                
+                EditorGUI.DrawRect(bgRect, backgroundColor);
+                EditorGUI.LabelField(new Rect(selectionRect.position + offset, selectionRect.size), obj.name, new GUIStyle()
+                {
+                    normal = new GUIStyleState()
+                    {
+                        textColor = textColor
+                    },
+                });
+                if(texture != null)
+                    EditorGUI.DrawPreviewTexture(new Rect(selectionRect.position, new Vector2(selectionRect.height, selectionRect.height)), texture);
+            }
+            
         }
-        
-        // if (obj != null)
-        // {
-        //     var prefabType = PrefabUtility.GetPrefabAssetType(obj);
-        //     
-        //     Rect offsetRect = new Rect(selectionRect.position + new Vector2(17.2f, 0), selectionRect.size);
-        //     EditorGUI.LabelField(offsetRect, obj.name, new GUIStyle()
-        //     {
-        //         normal = new GUIStyleState() { textColor = Color.cyan },
-        //         fontStyle = styleFont
-        //     });
-        // }
-        
     }
 }
