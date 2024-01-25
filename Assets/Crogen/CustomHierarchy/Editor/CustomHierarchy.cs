e44e8c2927630ff55f24f97dd551ada5889292a8
Crogen0407 <mymcoding0407@gmail.com>
2024-01-25 23:00:12 +0900

    feat : Hierarchy Color

--- Assets/Crogen/CustomHierarchy/Editor/CustomHierarchy.cs
+++ Assets/Crogen/CustomHierarchy/Editor/CustomHierarchy.cs
@@ -1,6 +1,7 @@
 using System;
 using Crogen.CustomHierarchy;
 using Crogen.CustomHierarchy.Editor;
+using Unity.VisualScripting;
 using UnityEngine;
 using Color = UnityEngine.Color;
 
@@ -18,49 +19,102 @@ public class CustomHierarchy : MonoBehaviour
         EditorApplication.hierarchyWindowItemOnGUI += HandleHierarchyOnGUI;
     }
 
+    ~CustomHierarchy()
+    {
+        EditorApplication.hierarchyWindowItemOnGUI -= HandleHierarchyOnGUI;
+    }
+
     private static void HandleHierarchyOnGUI(int instanceID, Rect selectionRect)
     {
-        Texture2D texture = null;
-        
         var obj = EditorUtility.InstanceIDToObject(instanceID);
-        if (((GameObject)obj) != null)
-        {
-            var components = ((GameObject)obj).GetComponents<Component>();
-            if (components != null)
-            {
-                Type t = null;
-                string guid;
-                long file;
-                // foreach (var component in components)
-                // {
-                //     t = component.GetType();
-                //     if (UnityEditor.AssetPreview.IsLoadingAssetPreview(component.GetInstanceID()))
-                //     {
-                //     }
-                //
-                //     AssetDatabase.TryGetGUIDAndLocalFileIdentifier(obj, out guid, out file);
-                //     var path = AssetDatabase.GetAssetPath(components[0].GetInstanceID());
-                //
-                //     Debug.Log(texture);
-                // }
-            }
 
-            texture = HierarchyIcon.LoadIcon("BuildSettings.Editor");
-        }
-
-
-        Color backgroundColor = Color.white;
-        Color textColor = Color.white;
-          
         if (obj != null)
         {
+            //MonoBehaviour
+            var gameObject = (GameObject)obj;
+            var parent = gameObject.transform.parent;
+            var components = gameObject.GetComponents<Component>();
             
-            if (obj.name.StartsWith("="))
+            //Hierarchy Visual
+            int hierarchyIndex = ((int)selectionRect.position.y / 16) % 2; //0 : 연한 거 / 1 : 진한 거
+            
+            #region Draw Background
+            
+            //MainBackground
+            Color backgroundColor = Color.white;
+            Rect backgroundPosition = new Rect(new Vector2(32, selectionRect.y), new Vector2(1000, selectionRect.height));
+        
+            if (hierarchyIndex == 0)
             {
-                backgroundColor = new Color(0,1,1,0.35f);
+                backgroundColor = new Color32(65, 65, 65, 100);
             }
+            else
+            {
+                backgroundColor = new Color32(56, 56, 56, 100);
+            }
+            EditorGUI.DrawRect(backgroundPosition, backgroundColor);
             
-            #region DrawLine
+            #region Draw Gradient
+
+            Rect gradientBackgroundPosition = new Rect(new Vector2(32, selectionRect.y), new Vector2(200, selectionRect.height));
+            Texture2D gradientTexture = (Resources.Load("GradientHorizontal") as Texture2D);
+            GUI.DrawTexture(gradientBackgroundPosition, gradientTexture, ScaleMode.ScaleAndCrop, true, 0, Color.red, Vector4.zero, 0);
+            
+
+            #endregion
+            
+            #endregion
+
+            #region Draw Setting Icon
+
+            
+            
+
+            #endregion
+            
+            #region Draw Icon
+
+            Rect iconPosition = new Rect(selectionRect.position, new Vector2(selectionRect.height, selectionRect.height));
+            //
+            // if (obj.name.StartsWith("="))
+            // {
+            //     Color iconBackgroundColor = Color.white;
+            //     
+            //     //Setting Icon
+            //     Texture2D iconTexture = HierarchyIcon.LoadIcon("BuildSettings.Editor");
+            //     
+            //     //Icon Background
+            //     if (Selection.activeGameObject == gameObject)
+            //     {
+            //         if (hierarchyIndex % 2 == 0)
+            //         {
+            //             iconBackgroundColor = new Color32(52,82,108,255);
+            //
+            //         }
+            //         else
+            //         {
+            //             iconBackgroundColor = new Color32(49, 78, 104, 255);
+            //         }
+            //     }
+            //     else
+            //     {
+            //         iconBackgroundColor = new Color32(56, 56, 56, 255);
+            //         EditorGUI.DrawRect(iconPosition,new Color32(56,56,56,255));
+            //     }
+            //     EditorGUI.DrawRect(iconPosition, iconBackgroundColor);
+            //     
+            //     //Icon
+            //     GUI.DrawTexture(iconPosition, iconTexture);
+            // }
+            // else
+            // {
+            Texture2D defaultIconTexture = HierarchyIcon.LoadIcon("d_GameObject Icon");
+            GUI.DrawTexture(iconPosition, defaultIconTexture);
+            // }
+
+            #endregion
+            
+            #region Draw Line
 
             if (Mathf.Approximately(selectionRect.position.x, 60f) == false)
             {
@@ -69,27 +123,55 @@ public class CustomHierarchy : MonoBehaviour
                 float count = (selectionRect.position.x - 60) / 14;
                 for (int i = 0; i < count; i++)
                 {
-                    EditorGUI.DrawRect(new Rect(selectionRect.position + new Vector2(-8.25f - 14 * (i + 1),-8), new Vector2(2, 16)), lineColor);
-                    EditorGUI.DrawRect(new Rect(selectionRect.position + new Vector2(-8.25f - 14,8), new Vector2(16, 2)), lineColor);
+                    //HorizontalLine
+                    if (gameObject.transform.childCount != 0)
+                    {
+                        EditorGUI.DrawRect(new Rect(selectionRect.position + new Vector2(-8.25f - 14,8), new Vector2(8, 2)), lineColor);
+                    }
+                    else
+                    {
+                        EditorGUI.DrawRect(new Rect(selectionRect.position + new Vector2(-8.25f - 14,8), new Vector2(16, 2)), lineColor);
+                    }
+                    
+                    
+                    //VerticalLine
+                    if (parent.GetChild(0) == gameObject.transform && i == 0)
+                    {
+                        EditorGUI.DrawRect(new Rect(selectionRect.position + new Vector2(-8.25f - 14 * (i + 1),0), new Vector2(2, 16)), lineColor);
+                    }
+                    else
+                    {                                        
+                        EditorGUI.DrawRect(new Rect(selectionRect.position + new Vector2(-8.25f - 14 * (i + 1),-8), new Vector2(2, 16)), lineColor);
+                    }
                 }
+
             }
 
             #endregion
-            
-            if (backgroundColor != Color.white)
-            {
-                EditorGUI.DrawRect(new Rect(selectionRect.position, new Vector2(selectionRect.height, selectionRect.height)),new Color32(56,56,56,255) );
 
-                // Rect offsetRect = new Rect(selectionRect.position + offset, selectionRect.size);
-                // Rect bgRect = new Rect(selectionRect.x, selectionRect.y, selectionRect.width + 50,
-                //     selectionRect.height);
-                // EditorGUI.DrawRect(bgRect, backgroundColor);
-                
-                if (texture != null)
+            #region Draw Text
+
+            GUIStyle textStyle = null;
+            textStyle = new GUIStyle()
+            {
+                normal = new GUIStyleState()
                 {
-                    GUI.DrawTexture(new Rect(selectionRect.position, new Vector2(selectionRect.height, selectionRect.height)), texture);
+                    textColor = gameObject.activeSelf == true ? Color.white : Color.gray
                 }
-            }
+            };
+            
+            GUI.Box(new Rect(selectionRect.position + new Vector2(17.9f,0), selectionRect.size), obj.name, textStyle);
+            
+            #endregion
+
+            #region Draw Toggle
+
+            Rect togglePosition = new Rect(new Vector2(32, selectionRect.y), new Vector2(selectionRect.height, selectionRect.height));
+            gameObject.SetActive(GUI.Toggle(togglePosition, gameObject.activeSelf, ""));
+
+            #endregion
+
+            
         }
     }
 }
