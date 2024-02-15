2cb8bf2af3fd8c7100e63964f3b59ed35ab1fb00
Crogen0407 <mymcoding0407@gmail.com>
2024-02-15 21:46:49 +0900

    feat : PowerfulInput

---
+++ Assets/Crogen/PowerfulInput/InputReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Crogen.PowerfulInput
+{
+    [CreateAssetMenu(fileName = "InputReader", menuName = "Crogen/InputReader", order = 0)]
+    public class InputReader : ScriptableObject, Controls.IPlayerActions
+    {
+        #region Input Event
+
+        public event Action<Vector3> MoveEvent;
+        public event Action DashEvent;
+        public event Action AttackEvent;
+
+        #endregion
+        
+        public void OnDash(InputAction.CallbackContext context)
+        {
+            if(context.performed)
+                DashEvent?.Invoke();
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            MoveEvent?.Invoke(context.ReadValue<Vector3>());
+        }
+
+        public void OnAttack(InputAction.CallbackContext context)
+        {
+            if(context.performed)
+                AttackEvent?.Invoke();
+        }
+    }
+}
